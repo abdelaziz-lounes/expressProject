6376b6bfc5021de7fec15527070a66b678662b47
abdelaziz-lounes <abdelazizlounes2@gmail.com>
2023-09-18 15:11:37 +0100

    adding template engine for dynamic rendring and veiws folder

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const freindsRouter= require("./routers/freinds.router");
 const messagesRouter= require('./routers/messages.router')
 
 const app = express();
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 const PORT = 3000;
 
@@ -17,7 +19,15 @@ app.use(function(req, res,next){
   console.log(`logging ${req.url} ${req.method} ${delta}ms`)
 })
 
+app.use( express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'My Friends Are VERYY Clever',
+    caption: 'Let\'s go skiing!',
+  });
+});
 
 //end points
 app.use('/freinds',freindsRouter);
